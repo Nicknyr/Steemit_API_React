1d8e5235cba584feaac50f9663fc3732e36a9af0
Nick Kinlen <nick_kinlen@hotmail.com>
2018-06-19 01:12:50 -0400

    Successfully display HTML UL with data from API call in GetPosts component

--- src/App.js
+++ src/App.js
@@ -5,41 +5,14 @@ import axios from 'axios';
 import steem from 'steem';
 import dsteem from 'dsteem';
 import { Client } from 'dsteem';
+import GetPosts from './GetPosts';
 
 class App extends Component {
-  constructor(props) {
-    super(props);
-
-    this.state = {
-      userData: ['hi']
-    }
-  }
-
-
-  componentDidMount() {
-    const client = new Client('https://api.steemit.com')
-
-    var query = {
-        tag: 'steemitblog', // This tag is used to filter the results by a specific post tag
-        limit: 5, // This limit allows us to limit the overall results returned to 5
-    };
-
-    client.database
-       .getDiscussions('blog', query)
-       .then(result => {
-         this.setState({ userData: result});
-       });
-  }
-
-
 
   render() {
-  // const data = this.state.userData;
-  //  console.log("Hi I am data" + data);
-   console.log(this.state.userData);
     return (
-      <div className="App">
-        {this.setState.userData}
+      <div className="app-container">
+        <GetPosts />
       </div>
     );
   }
---
+++ src/GetPosts.js
@@ -0,0 +1,56 @@
+import React, { Component } from 'react';
+import dsteem from 'dsteem';
+import { Client } from 'dsteem';
+
+class GetPosts extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      userData: []
+    }
+  }
+
+  componentDidMount() {
+    const client = new Client('https://api.steemit.com')
+
+    var query = {
+        tag: 'utopian-io', // This tag is used to filter the results by a specific post tag
+        limit: 5, // This limit allows us to limit the overall results returned to 5
+    };
+
+    client.database
+       .getDiscussions('blog', query)
+       .then(result => {
+         this.setState({ userData: result});
+       });
+  }
+
+
+  render() {
+   console.log(this.state.userData);
+
+   const data = this.state.userData;
+
+   const display = Object.keys(data).map((d, key) => {
+     console.log(data[key]);
+     return (
+       <div className="blog-posts">
+        <li key={key}>
+          {data[key].author}
+        </li>
+       </div>
+     );
+   })
+
+    return (
+      <div className="App">
+        <ul>
+          {display}
+        </ul>
+      </div>
+    );
+  }
+}
+
+export default GetPosts;
