827b718c4784c4d82891b0477c9fe76c6f0b6e95
Nick Kinlen <nick_kinlen@hotmail.com>
2018-06-20 21:03:59 -0400

    Incorporated FCC solution to nested objects problem. App now displays posts from Steemit

--- src/MyPosts.js
+++ src/MyPosts.js
@@ -1,6 +1,7 @@
-import React, { Component } from 'react';
+/*import React, { Component } from 'react';
 import dsteem from 'dsteem';
 import { Client } from 'dsteem';
+import lodash from 'lodash';
 
 
 class MyPosts extends Component {
@@ -8,7 +9,8 @@ class MyPosts extends Component {
     super(props);
 
     this.state = {
-      myPosts: []
+      myPosts: [],
+      nestedObject: []
     }
   }
 
@@ -19,29 +21,23 @@ class MyPosts extends Component {
       .getState('@ned')
       .then(result => {
         this.setState({ myPosts: result });
+        this.setState({ nestedObject: result.content});
       })
   }
 
   render() {
-    console.log(this.state.myPosts);
+    //console.log(this.state.myPosts);
 
-    const data = this.state.myPosts;
+    const posts = this.state.nestedObject;
 
-    const display = Object.keys(data).map((d, key) => {
-    return (
-      <div className="my-posts">
-        <li key={key}>
-          {data.current_route}
-        </li>
-      </div>
-      );
-    });
+    const display = Object.keys(posts).map(key =>
+      <option value={key}>{posts[key]}</option>
+    )
+
 
     return(
       <div>
-        <ul>
-          { display }
-        </ul>
+        {display}
       </div>
     );
   }
@@ -50,5 +46,62 @@ class MyPosts extends Component {
 
 }
 
+
+export default MyPosts;
+*/
+
+
+
+
+
+import React, { Component } from "react";
+import dsteem from "dsteem";
+import { Client } from "dsteem";
+
+class MyPosts extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      myPosts: []
+    };
+  }
+
+  componentDidMount() {
+    const client = new Client("https://api.steemit.com");
+
+    client.database.getState("@ned").then(result => {
+      this.setState({ myPosts: result.content });
+    });
+  }
+
+  render() {
+    const posts = Object.keys(this.state.myPosts);
+    console.log(posts);
+    const onePost = this.state.myPosts[posts[0]];
+    console.log(onePost)
+    let page = posts.map((post, i) => {
+      return (
+        <div style={{ margin: "15px 15px 60px 15px " }}>
+          <p>
+            <strong>Author: {this.state.myPosts[posts[i]]["author"]}</strong>
+          </p>
+          <p>{this.state.myPosts[posts[i]]["permlink"]}</p>
+          <p>{this.state.myPosts[posts[i]]["created"]}</p>
+          <p>{this.state.myPosts[posts[i]]["body"]}</p>
+        </div>
+      );
+    });
+
+    // for (let i = 0; i < posts.length; i++) {
+    //   console.log(this.state.myPosts[posts[i]]);
+    //   console.log(Object.keys(this.state.myPosts[posts[i]]));
+    //   console.log(this.state.myPosts[posts[i]]["author"]);
+    //   console.log(this.state.myPosts[posts[i]]["body"]);
+    // }
+
+    return <div>{page}</div>;
+  }
+}
 
 export default MyPosts;
